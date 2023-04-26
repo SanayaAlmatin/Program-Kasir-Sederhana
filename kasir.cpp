9a3022f925d728693ddd93d72635f1b71f91fcff
SanayaAlmatin <sanayaalmatin@gmail.com>
2023-04-26 21:01:16 +0700

    Delete price list

--- kasir.cpp
+++ kasir.cpp
@@ -9,30 +9,24 @@ int main() {
 	float price_item, Tp1, Tp2, cash, change;
 	char opt = 'Y';
 	
-	cout<<"Price List Item : ";
-	cout<<"\n1. Biore Body Wash = Rp 2000 \n2. Rinso = Rp 11000 \n3. Indomie Goreng Rendang = Rp 3000"<<endl;
-	
-	
 	change = cash - Tp2;
 
 	while ((opt=='Y')||(opt=='y')){
-	
+		
 		cout<<"\nInput the goods name: ";
 		cin.ignore();
 		getline (cin,goods_name);
-		cout<<"\nQuantity : ";
+		cout<<"Quantity : ";
 		cin>>quantity;
-		cout<<"\nPrice per item : ";
+		cout<<"Price per item : ";
 		cin>>price_item;
 		Tp1 = quantity * price_item;
-		cout<<"\nTotal price : "<<Tp1;
+		cout<<"Total price : "<<Tp1<<'\n';
 		
 		Tp2 = Tp2 + Tp1;
 		
-		
 		cout<<"\nDo you want to enter more numbers (y/n)? ";
 		cin>>opt;
-	
 	}
 	cout<<"\nTotal : "<<Tp2;
 	cout<<"\nCash : ";
@@ -40,5 +34,5 @@ int main() {
 	change = cash - Tp2;
 	cout<<"Change : "<<change;
 
-
+	return 0;
 }
